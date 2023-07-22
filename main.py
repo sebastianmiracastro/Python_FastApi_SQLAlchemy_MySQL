b07b6471d385d4254757cf131492b160aa78462e
sebastianmiracastro <smira44@misena.edu.co>
2023-07-21 21:09:26 -0500

    Fixing foreign key relationships #3 [20230721]

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI;
 
+# IMPORT ENGINE DATABASE
+
+from service.databaseConfig import create_engine
+
 # UTILS TABLES DATABASE
 
 from utils.usersTable import usuarios
+from utils.servicesTable import servicios
+from utils.subscriptionsTable import suscripciones
 
 app = FastAPI()
 
--- utils/subscriptionsTable.py
+++ utils/subscriptionsTable.py
@@ -6,13 +6,18 @@ from service.databaseConfig import get_connection
 
 conn = get_connection()
 
+# FOREIGN KEY CONNECTIONS
+
+from utils.usersTable import usuarios
+from utils.servicesTable import servicios
+
 meta = MetaData()
 
 suscripciones = Table(
     'suscripciones', meta,
     Column('id_Suscripcion', Integer, primary_key=True, autoincrement=True),
-    Column('id_Usuario_suscripcion',Integer , ForeignKey('usuarios.id_Usuario') ,nullable=False),
-    Column('id_servicio_suscripcion', Integer, ForeignKey('servicios.id_Servicio'), nullable=False),
+    Column('id_Usuario_suscripcion',Integer , ForeignKey(usuarios.c.id_Usuario) ,nullable=False),
+    Column('id_servicio_suscripcion', Integer, ForeignKey(servicios.c.id_Servicio), nullable=False),
     Column('fecha_suscripcion', DateTime, nullable=False),
     Column('activa_suscripcion', Boolean),
     Column('fecha_desuscripcion', DateTime, nullable=True)
